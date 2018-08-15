7a7ebce65ed999a5d4daac6e7eaab97c8fe538da
Hyldig01 <33703671+Hyldig01@users.noreply.github.com>
2018-08-15 21:54:32 +0200

    arduino IDE file

---
+++ pirunderbedlight/pirunderbedlight.ino
@@ -0,0 +1,119 @@
+#include <Adafruit_NeoPixel.h>
+#include <avr/power.h>
+
+#define PIN 6
+
+
+Adafruit_NeoPixel strip = Adafruit_NeoPixel(60, 10, NEO_GRB + NEO_KHZ800);
+const byte motionPin = 2;
+byte senseMotion = 0; 
+
+
+void setup() {
+
+
+
+  strip.begin();
+  strip.show();
+pinMode(motionPin, INPUT) ;
+}
+
+void loop() {
+ 
+  senseMotion = digitalRead(motionPin);
+     if (senseMotion == HIGH) { 
+  colorWipe(strip.Color(255, 0, 0), 50); 
+    delay(9000);
+  colorWipe(strip.Color(0, 0, 0), 50); 
+
+}
+}
+
+
+
+void colorWipe(uint32_t c, uint8_t wait) {
+  for(uint16_t i=0; i<strip.numPixels(); i++) {
+      strip.setPixelColor(i, c);
+      strip.show();
+      delay(wait);
+  }
+}
+
+void rainbow(uint8_t wait) {
+  uint16_t i, j;
+
+  for(j=0; j<256; j++) {
+    for(i=0; i<strip.numPixels(); i++) {
+      strip.setPixelColor(i, Wheel((i+j) & 255));
+    }
+    strip.show();
+    delay(wait);
+  }
+}
+
+/
+void rainbowCycle(uint8_t wait) {
+  uint16_t i, j;
+
+  for(j=0; j<256*5; j++) { 
+    for(i=0; i< strip.numPixels(); i++) {
+      strip.setPixelColor(i, Wheel(((i * 256 / strip.numPixels()) + j) & 255));
+    }
+    strip.show();
+    delay(wait);
+  }
+}
+
+
+void theaterChase(uint32_t c, uint8_t wait) {
+  for (int j=0; j<10; j++) {  //do 10 cycles of chasing
+    for (int q=0; q < 3; q++) {
+      for (int i=0; i < strip.numPixels(); i=i+3) {
+        strip.setPixelColor(i+q, c);    
+      }
+      strip.show();
+     
+      delay(wait);
+     
+      for (int i=0; i < strip.numPixels(); i=i+3) {
+        strip.setPixelColor(i+q, 0);        
+      }
+    }
+  }
+}
+
+
+void theaterChaseRainbow(uint8_t wait) {
+  for (int j=0; j < 256; j++) {    
+    for (int q=0; q < 3; q++) {
+        for (int i=0; i < strip.numPixels(); i=i+3) {
+          strip.setPixelColor(i+q, Wheel( (i+j) % 255));    
+        }
+        strip.show();
+       
+        delay(wait);
+       
+        for (int i=0; i < strip.numPixels(); i=i+3) {
+          strip.setPixelColor(i+q, 0);        
+        }
+    }
+  }
+}
+
+
+uint32_t Wheel(byte WheelPos) {
+  WheelPos = 255 - WheelPos;
+  if(WheelPos < 85) {
+   return strip.Color(255 - WheelPos * 3, 0, WheelPos * 3);
+  } else if(WheelPos < 170) {
+    WheelPos -= 85;
+   return strip.Color(0, WheelPos * 3, 255 - WheelPos * 3);
+  } else {
+   WheelPos -= 170;
+   return strip.Color(WheelPos * 3, 255 - WheelPos * 3, 0);
+  }
+}
+
+
+
+
